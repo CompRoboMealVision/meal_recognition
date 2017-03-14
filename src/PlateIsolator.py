099c6b7e906f7b8972d74098cb6dfe805fa3fd39
Jonah Spear <Jonahspear512@gmail.com>
2017-03-14 17:46:29 -0400

    removed unnecessary code

--- src/PlateIsolator.py
+++ src/PlateIsolator.py
@@ -42,31 +42,11 @@ def isolatePlate(image, canny_thresh1=100, canny_thresh2=200, num_contours=10, n
     gx = cv2.Sobel(image_gray,cv2.CV_64F,1,0,ksize=5)
     gy = cv2.Sobel(image_gray,cv2.CV_64F,0,1,ksize=5)
     final_image = drawNormals(image_with_windows, window_xs, window_ys, gx, gy)
-    thetas = calcTangents(window_xs, window_ys, gx, gy)
 
     x1 = window_xs[0]
     y1 = window_ys[0]
-    # angle = thetas[0]
 
-    # left = -100
-    # x_left = np.cos(angle)*left + x1
-    # y_left = np.sin(angle)*left + y1
-    # right = 1000
-    # x_right = np.cos(angle)*right + x1
-    # y_right = np.sin(angle)*right + y1
-    # # import pdb; pdb.set_trace()
-
-    # m = (y_left - y_right)/(x_left - x_right)
     cv2.circle(final_image, (x1, y1), 3, (0, 255, 255), 3)
-
-    # cv2.line(final_image, (int(x_left), int(y_left)), (int(x_right), int(y_right)), color=(255, 0, 0), thickness=2)
-
-    
-    # for (x2, y2) in zip(window_xs, window_ys):
-    #     if inRegion(m, (x_left, y_left), (x2, y2)):
-    #         # Draw a circle at that point
-    #         # We pass in (y2, x2) because that corresponds to (row, col)
-    #         cv2.circle(final_image, (x2, y2), 1, (0, 255, 0), 2)
 
     normal_vec_x = gx[y1, x1]
     normal_vec_y = gy[y1, x1]
@@ -79,8 +59,6 @@ def isolatePlate(image, canny_thresh1=100, canny_thresh2=200, num_contours=10, n
             # We pass in (y2, x2) because that corresponds to (row, col)
             cv2.circle(final_image, (x2, y2), 1, (0, 255, 0), 2)
 
-
-    print thetas
     return final_image
 
 
@@ -109,15 +87,6 @@ def drawWindows(image, window_xs, window_ys, width=20):
 
     return im
 
-def calcTangents(window_xs, window_ys, gx, gy):
-    """ Calcs slope of tangent line at window coordinates. """
-    grad_angle = np.arctan2(gy, gx)
-    thetas = []
-    for (col, row) in zip(window_xs, window_ys):
-        theta = grad_angle[row, col] + np.pi/2
-        thetas.append(theta)
-    return thetas
-
 def drawNormals(image, window_xs, window_ys, gx, gy):
     """ Draws normals at window coordinates. """
     im = np.copy(image)
@@ -139,14 +108,6 @@ def inDirection(normal, origin, point):
     diff = point - origin
     double_dot = np.dot(diff, normal)
     return double_dot > 0
-
-def inRegion(m, (x1, y1), (x2, y2)):
-    """ Detect if a point (x2, y2) is in a region bounded below 
-        by the line defined by m, x1, and y1. """
-    # Calculate the slope of our bounding line
-    b = y1 - m*x1
-    return y2 >= m*x2 + b
-
 
 
 if __name__ == '__main__':
