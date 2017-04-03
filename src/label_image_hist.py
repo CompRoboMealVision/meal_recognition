f006d7e88bfdbc4d212ca82b766c7a920359a5f6
Xiaozheng Xu <xiaozheng.xu@students.olin.edu>
2017-04-03 13:05:52 -0400

    Adding more pictures of food

--- src/label_image_hist.py
+++ src/label_image_hist.py
@@ -3,6 +3,7 @@
 from label_image import label_image, setup
 import sys
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import numpy as np
 
 def plot_bar_graph(labels, scores, bins):
@@ -19,11 +20,28 @@ def plot_bar_graph(labels, scores, bins):
     ax.set_title('Top 5 food categories probablities')
     plt.show()
 
-    
+def plot_bar_graph_withfood(image_path, labels, scores, bins):
+    ax1 = plt.subplot(211)
+    img = mpimg.imread(image_path)
+    fig = plt.imshow(img)
+
+    fig.axes.get_xaxis().set_visible(False)
+    fig.axes.get_yaxis().set_visible(False)
+
+    ax = plt.subplot(212)
+    ind = np.arange(bins)
+    width = 0.35
+    labels = labels[0:bins]
+    heights = scores[0:bins]
+    rects = ax.bar(ind, heights, width)
+    ax.set_xticks(ind)
+    ax.set_xticklabels((labels))
+    ax.set_ylabel('Probablity')
+    plt.show()
 
 if __name__ == '__main__':
     # change this as you see fit
     image_path = sys.argv[1]
     human_strings = setup()
     labels, scores = label_image(image_path, human_strings)
-    plot_bar_graph(labels, scores, 5)
+    plot_bar_graph_withfood(image_path, labels, scores, 5)
